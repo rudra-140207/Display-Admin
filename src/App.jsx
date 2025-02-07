9c3f47f7ddc112b071baa9a147d239c739ab496f
RUDRA PRATAP SINGH <rudra.2226en1029@kiet.edu>
2025-02-07 16:30:05 +0530

    initila commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AdminPanel from './components/AdminPanel';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<AdminPanel />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
