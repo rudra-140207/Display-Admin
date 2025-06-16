69d04956c517175d16d17f781df78c3b830ce764
RUDRA PRATAP SINGH <rudra.2226en1029@kiet.edu>
2025-06-16 22:01:48 +0530

    updated firebase credentials to env

--- src/common/firebase.js
+++ src/common/firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, onValue } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOqjZC0tDtKpVWEzXbWyQDGw6zEBlhynU",
-  authDomain: "en-ac-e6057.firebaseapp.com",
-  databaseURL: "https://en-ac-e6057-default-rtdb.firebaseio.com",
-  projectId: "en-ac-e6057",
-  storageBucket: "en-ac-e6057.firebasestorage.app",
-  messagingSenderId: "336246921508",
-  appId: "1:336246921508:web:7480d624b71f442a70bbb1"
+  apiKey: import.meta.env.apiKey,
+  authDomain: import.meta.env.authDomain,
+  databaseURL: import.meta.env.databaseURL,
+  projectId: import.meta.env.projectId,
+  storageBucket: import.meta.env.storageBucket,
+  messagingSenderId: import.meta.env.messagingSenderId,
+  appId: import.meta.env.appId,
 };
 
 const app = initializeApp(firebaseConfig);
